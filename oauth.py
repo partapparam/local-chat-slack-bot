4937133db53877f9e0ee2dcb2f91ff27682a99f5
partapparam <paramsingh1961@gmail.com>
2024-03-25 21:30:45 -0700

    full scopes

--- oauth.py
+++ oauth.py
@@ -4,6 +4,7 @@ from slack_bolt.oauth.oauth_settings import OAuthSettings
 from slack_sdk.oauth.installation_store import FileInstallationStore
 from slack_sdk.oauth.state_store import FileOAuthStateStore
 from dotenv import load_dotenv
+from typing import Any, List
 load_dotenv()
 import yaml
 
@@ -11,13 +12,16 @@ SLACK_CLIENT_SECRET = os.getenv(key='SLACK_CLIENT_SECRET')
 SLACK_CLIENT_ID = os.getenv(key='SLACK_CLIENT_ID')
 # Get SLack Scopes from yaml file
 # will eb a string
-with open(file='./src/templates/scopes.yaml', mode='r') as file:
-    config = yaml.safe_load(file) 
+def get_scopes() -> List:
+    with open(file='./src/templates/scopes.yaml', mode='r') as file:
+        config = yaml.safe_load(file) 
+    scopes_list = config['scopes']['user'] + config['scopes']['bot']
+    return scopes_list
 
 oauth_settings = OAuthSettings(
     client_id=SLACK_CLIENT_ID,
     client_secret=SLACK_CLIENT_SECRET,
-    scopes=["channels:read", "groups:read", "chat:write"],
+    scopes=get_scopes(),
     installation_store=FileInstallationStore(base_dir="./data/installations"),
     state_store=FileOAuthStateStore(expiration_seconds=600, base_dir="./data/states")
 )
